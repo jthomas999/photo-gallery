10e9aa724c46ea17485abab931a20147760444e5
jthomas999 <61428653+jthomas999@users.noreply.github.com>
2020-04-29 20:29:50 -0400

    Create useFirebase.ts

---
+++ src/hooks/useFirebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+export function useFirebase() {
+
+  const userLogin = async (email:any ,password:any) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(resp=>{ console.log(resp); },err=>{ console.log(err); })
+  };
+
+  return {
+    userLogin
+  };
+}
